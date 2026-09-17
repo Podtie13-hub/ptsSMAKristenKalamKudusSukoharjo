a48188ccdc3e1bc270aecae2896de3c2e19ffefe
Podtie13-hub <novian@skkksurakarta.sch.id>
2026-09-17 10:25:17 +0700

    Update LoginPage.tsx

--- src/components/auth/LoginPage.tsx
+++ src/components/auth/LoginPage.tsx
@@ -80,7 +80,7 @@ export const LoginPage: React.FC<LoginPageProps> = ({
       }
 
       // Fallback check for podtie13 / Zionathan123
-      if (cleanUser === 'podtie13' && cleanPass === 'Zionathan123') {
+      if (cleanUser === 'isi bagian ini' && cleanPass === 'isi bagian ini') {
         onLogin({
           role: 'admin',
           username: 'podtie13',
